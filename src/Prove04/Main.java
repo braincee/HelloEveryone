16f8b89ebac4e0aab799825aa14978cfec470cbe
Kel <kel.biesinger@gmail.com>
2021-01-29 19:06:16 +0100

    Teammate edit

--- src/Prove04/Main.java
+++ src/Prove04/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Stephen");
+        System.out.println("Hello Stephen, my name is Kel");
     }
 }
